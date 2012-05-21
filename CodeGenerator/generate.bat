d9f2ba66b65a8e7797d6561a4f0129cb9b285289
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-05-21 20:36:37 +0800

    Added Code Generator

---
+++ CodeGenerator/generate.bat
@@ -0,0 +1,11 @@
+set connectionstring="Data Source=.\ALPHA;Initial Catalog=Meek.Commerce.DEMO;Integrated Security=True"
+set namespace="Meek.Commerce.Framework.Model"
+set language="CSharp"
+set outputobjectlayer="output\Entities.cs"
+set outputssdl="output\Meek.Commerce.ssdl"
+set outputcsdl="output\Meek.Commerce.csdl"
+set outputmsl="output\Meek.Commerce.msl"
+set outputview="output\View.cs"
+set entitycontainer="DataContext"
+
+EdmGen /mode:fullgeneration /connectionstring:%connectionstring% /namespace:%namespace% /language:%language% /outobjectlayer:%outputobjectlayer% /outssdl:%outputssdl% /outcsdl:%outputcsdl% /outmsl:%outputmsl% /outviews:%outputview% /entitycontainer:%entitycontainer% /nologo
