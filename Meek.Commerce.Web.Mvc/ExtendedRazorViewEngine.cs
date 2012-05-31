0a9ee6f3a26e507cc2cf2be426e9229b997ef05c
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-05-31 17:33:36 +0800

    Modified ExtendedRazorViewEngine.cs

--- Meek.Commerce.Web.Mvc/ExtendedRazorViewEngine.cs
+++ Meek.Commerce.Web.Mvc/ExtendedRazorViewEngine.cs
@@ -6,51 +6,7 @@ using System.Web.Mvc;
 
 namespace Meek.Commerce.Web.Mvc
 {
-    public class ExtendedRazorViewEngine : RazorViewEngine
+    public class ExtendedRazorViewEngine : Meek.Web.Mvc.ExtendedRazorViewEngine
     {
-        public ExtendedRazorViewEngine() 
-        {
-            //Areas
-            AreaViewLocationFormats = new[] {
-                "~/Areas/{2}/{1}/{0}.cshtml",
-                "~/Areas/{2}/{1}/{0}.vbhtml",
-                "~/Areas/{2}/Shared/{0}.cshtml",
-                "~/Areas/{2}/Shared/{0}.vbhtml"
-            };
-
-            AreaMasterLocationFormats = new[] {
-                "~/Areas/{2}/{1}/{0}.cshtml",
-                "~/Areas/{2}/{1}/{0}.vbhtml",
-                "~/Areas/{2}/Shared/{0}.cshtml",
-                "~/Areas/{2}/Shared/{0}.vbhtml"
-            };
-
-            AreaPartialViewLocationFormats = new[] {
-                "~/Areas/{2}/{1}/{0}.cshtml",
-                "~/Areas/{2}/{1}/{0}.vbhtml",
-                "~/Areas/{2}/Shared/{0}.cshtml",
-                "~/Areas/{2}/Shared/{0}.vbhtml"
-            };
-
-            //Root
-            ViewLocationFormats = new [] {
-                "~/{0}.cshtml",
-                "~/{0}.vbhtml",
-                "~/{1}/{0}.cshtml",
-                "~/{1}/{0}.vbhtml"
-            };
-
-            PartialViewLocationFormats = new[]{
-                "~/Controls/{0}.cshtml",
-                "~/Controls/{0}.vbhtml"
-            };
-
-            MasterLocationFormats = new[] {
-                "~/{0}.cshtml",
-                "~/{0}.vbhtml",
-                "~/Shared/{0}.cshtml",
-                "~/Shared/{0}.vbhtml"
-            };
-        }
     }
 }
